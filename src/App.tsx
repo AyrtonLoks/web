e1e2c0cd2b53a69e152f63ade14d4cc393fbc561
Ayrton Neves <ayrton.leandro@gmail.com>
2020-06-03 18:42:42 -0300

    fixing bugs

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import Header from './Header';
+// JSX permite usarmos a sintaxe de XML dentro do JS
+
+function App() {
+  return (
+    <div>
+      <Header title="Hello, World!"/>
+      <h1>Hello, World!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface HeaderProps {
+    title?: String;
+};
+
+const Header: React.FC<HeaderProps> = () => {
+    return (
+        <header>
+            <h1>Ecoleta</h1>
+        </header>
+    );
+}
+
+export default Header;
